7f50c43c621e566742657302f0ba717f9f610428
Geonwoo Lee <gwlee@kakao.com>
2022-09-13 19:05:35 +0900

    진행이력 팝업

--- src/js/_popupTrigger.js
+++ src/js/_popupTrigger.js
@@ -13,7 +13,7 @@
         const popupX = curX + curWidth / 2 - popupWidth / 2;
         const popupY = curY + curHeight / 2 - popupHeight / 2;
 
-        const opt = `width=${popupWidth}, height=${popupHeight}, left=${popupX}, top=${popupY}, scrollbars=0, toolbar=0, menubar=no`;
+        const opt = `width=100, height=100, left=${popupX}, top=${popupY}, scrollbars=0, toolbar=0, menubar=no`;
         const popup = window.open(that.href, "_blank", opt);
         popup.focus();
     };
