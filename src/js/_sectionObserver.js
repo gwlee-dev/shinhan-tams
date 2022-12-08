54245de4ecaf51186d5171fcf8c3859d92b50ed1
gwlee-dev <gwlee@kakao.com>
2022-12-08 15:47:45 +0900

    반입 직전

--- src/js/_sectionObserver.js
+++ src/js/_sectionObserver.js
@@ -3,28 +3,41 @@ import textDeliver from "./_textDeliver";
 import { tooltipEnabler } from "./_tooltip";
 
 const Section = class {
+    init = (target) => {
+        const targets = [...target.querySelectorAll("tbody, .hstack")];
+        targets
+            .map((el) => el.cloneNode(true))
+            .filter((el) => el?.id === "fdToolbar")
+            .forEach((el) => console.log(el));
+
+        targets.forEach((el) => {
+            this.observer.observe(el, {
+                childList: true,
+                characterData: true,
+                subtree: true,
+            });
+        });
+    };
+
     observer = new MutationObserver((mutationList, observer) => {
         const [mutationRecord] = mutationList;
+        console.log("요소추가 발생", mutationRecord.target);
         popupTrigger(mutationRecord.target);
         textDeliver();
         tooltipEnabler(mutationRecord.target);
+        this.init(mutationRecord.target);
     });
 
     constructor(section) {
-        const target = [
-            ...section.querySelectorAll("tbody, .hstack, .btn-group"),
-        ];
-        target.forEach((el) => {
-            this.observer.observe(el, {
-                childList: true,
-            });
-        });
+        this.init(section);
     }
 };
 
 const sectionObserver = () => {
     const doms = [
-        ...document.querySelectorAll("main > *:not(.contents), .contents > *"),
+        ...document.querySelectorAll(
+            "main > *:not(.contents), .contents > *, .popup > *"
+        ),
     ];
     doms.forEach((section) => new Section(section));
 };
